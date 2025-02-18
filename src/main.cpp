fd7013201f1d39087eed385c6ef5226e33f8605e
moukito <theofremont50@gmail.com>
2025-02-18 14:33:28 +0100

    Add MyDsp audio processing class and related DSP components

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <Audio.h>
+
+AudioInputI2S in; // replace mydsp and correspond to the microphone
+AudioOutputI2S out;
+AudioControlSGTL5000 audioShield;
+AudioConnection patchCord0(in,0,out,0);
+AudioConnection patchCord1(in,0,out,1);
 
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    AudioMemory(6);
+    audioShield.enable();
+    audioShield.inputSelect(AUDIO_INPUT_MIC);
+    audioShield.micGain(10); // in dB
+    audioShield.volume(0.3);
 }
 
 void loop() {
-// write your code here
+    delay(100);
 }
